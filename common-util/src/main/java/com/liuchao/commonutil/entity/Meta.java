6d23b9d0ed2df3692695bf71101d927ae014df78
dkws-2019 <liuchao@wondersgroup.com>
2019-08-28 10:02:06 +0800

    111

---
+++ common-util/src/main/java/com/liuchao/commonutil/entity/Meta.java
@@ -0,0 +1,49 @@
+package com.liuchao.commonutil.entity;
+
+public class Meta {
+    private String href;
+    private String icon;
+    private String title;
+    private String name;
+    private String hideInBread;
+
+    public String getHref() {
+        return href;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHideInBread() {
+        return hideInBread;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHideInBread(String hideInBread) {
+        this.hideInBread = hideInBread;
+    }
+}
